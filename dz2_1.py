f3c86bce91390935c4fe4da6a2688c50cd31550d
Роман Бухмистов <rrb13@mail.ru>
2022-09-30 22:05:28 +0300

    Доработана 1 задача

--- dz2_1.py
+++ dz2_1.py
@@ -3,21 +3,25 @@
 *Пример:*
 - 6782 -> 23
 - 0,56 -> 11'''
-print("***Программа суммы цифр цисла***")
-number=[]
-number=input("Введите вещественное или целое число: ")
+
+import os
+clear = lambda: os.system('clear')
+clear()
+print("***Программа суммы цифр числа***")
+
 def summ(num):
     res = [i for ele in num for i in ele]
-    # print(res)
     if ',' in res:
         res.remove(',')
     elif '.' in res:
         res.remove('.')
-    else:
-        print(res)
     sum=0
     for i in range(len(res)):
         sum+=int(res[i])
-    print("->", sum)
+    print("\n->", sum)
 
-summ(number)
+try:
+    number=input("\nВведите вещественное или целое число: ")
+    summ(number)
+except:
+    print("\nНужно вводить число!")
