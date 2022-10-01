14c0ce51c4be665c7d817a0085c287f73a8254cd
Роман Бухмистов <rrb13@mail.ru>
2022-10-01 23:46:51 +0300

    Написано решение 3 задачи

--- dz2_3.py
+++ dz2_3.py
@@ -3,3 +3,28 @@
 выводится на экран, затем перемешивается, опять выводится на экран. 
 SHUFFLE нельзя юзать!'''
 
+import os
+clear = lambda: os.system('clear')
+clear()
+
+print("***Программа перемешивания случайных чисел***\n")
+
+from random import randint
+list_length = 10 
+a,b = 10,99
+lst = [None] * list_length
+for i in range(list_length):
+    lst[i] = randint(a,b)
+print(lst)
+
+tmp = []
+while len(tmp) < list_length:
+    x = randint(0, 9)
+    if x not in tmp:
+        tmp.append(x)
+# print(tmp)
+
+lst_new = [None] * list_length
+for q in range(list_length):
+    lst_new[q] = lst[tmp[q]]
+print(lst_new)
