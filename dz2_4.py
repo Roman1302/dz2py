1540d61f0d270a1073625df72a83d7b9567c6b76
Роман Бухмистов <rrb13@mail.ru>
2022-10-02 19:36:37 +0300

    попытки доп задач

--- dz2_4.py
+++ dz2_4.py
@@ -1,3 +1,26 @@
 '''Задача 4 НЕОБЯЗАТЕЛЬНАЯ. Напишите программу, которая принимает на вход N, 
 и координаты двух точек и находит расстояние между ними в N-мерном пространстве.'''
 
+import os
+clear = lambda: os.system('clear')
+clear()
+print("***Программа находит расстояние между ними в N-мерном пространстве***")
+
+
+def array(np):
+    for i in range(np):
+        x=[]
+        y=[]
+        x.append(input("Введите x: "))
+        y.append(input("Введите y: "))
+        # print(lis)
+    return x, y
+nr=int(input("\nВведите N-мерном пространства: "))
+array(nr)
+print(array)
+
+# try:
+#     number=input("\nВведите вещественное или целое число: ")
+#     summ(number)
+# except:
+#     print("\nНужно вводить число!")
